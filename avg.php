3ebfdcea365b317175a725e6787cedfcf017d684
JoannaHerman <kamilasite@gmail.com>
2021-10-19 12:04:09 +0200

    Implementacja  funkcji sredniej wazonej

--- avg.php
+++ avg.php
@@ -5,6 +5,14 @@ function srednia(int a, int b){
 	return (a+b)/2;
 }
 
+function srednia-wazona(int a, int b){
+	resturn (7*a+3*b)/10;
+}
+
+//function wsrednia(int a, int b){
+//	return (8*a+2*b)/10;
+//}
+
 
 
 
